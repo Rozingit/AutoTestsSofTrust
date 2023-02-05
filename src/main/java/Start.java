26412b6cd2930f7200069b426b4345363ece576d
RuslanVasilev <rozinrus@gmail.com>
2023-02-05 23:31:41 +0300

    grut

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,5 +1,7 @@
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Привет!");
+        int a = 1;
+        System.out.println("Привет!" + a);
+
     }
 }
