6348343803905f1ce6eed9c4234270334d875182
RuslanVasilev <rozinrus@gmail.com>
2023-02-05 23:45:50 +0300

    Start project

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,7 +1,7 @@
 public class Start {
     public static void main(String[] args) {
         int a = 1;
-        System.out.println("Привет!" + a);
+        System.out.println("Привет!!" + a);
 
     }
 }
